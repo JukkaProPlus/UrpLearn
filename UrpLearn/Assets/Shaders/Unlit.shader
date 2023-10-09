1515804a0ddbd7d9bcfa72fb95193969f62f902c
jukka <jukka@DESKTOP-K8LQ3VJ>
2023-10-10 01:13:46 +0800

    .

--- UrpLearn/Assets/Shaders/Unlit.shader
+++ UrpLearn/Assets/Shaders/Unlit.shader
@@ -2,7 +2,7 @@
 {
     Properties
     {
-        
+        _BaseColor("Color", Color) = (1.0, 1.0, 1.0,1.0)       
     }
     SubShader
     {
@@ -12,6 +12,7 @@
             #pragma vertex UnlitPassVertex
             #pragma fragment UnlitPassFragment
             #include "./UnlitPass.hlsl"
+            // #include ""
 
             ENDHLSL
         }
--- UrpLearn/Assets/Shaders/UnlitPass.hlsl
+++ UrpLearn/Assets/Shaders/UnlitPass.hlsl
@@ -1,13 +1,17 @@
 #ifndef CUSTOM_UNLIT_PASS_INCLUDED
 #define CUSTOM_UNLIT_PASS_INCLUDED
+
+#include "../ShaderLibrary/Common.hlsl"
 //顶点函数
 float4 UnlitPassVertex(float3 positionOS:POSITION):SV_POSITION
 {
-    return float4(positionOS, 1.0);
+    float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+    return TransformWorldToHClip(positionWS);
 }
+float4 _BaseColor;
 //片元函数
-void UnlitPassFragment()
+float4 UnlitPassFragment():SV_TARGET
 {
-    
+    return _BaseColor;
 }
 #endif

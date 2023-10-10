3108dee5d3a98a117ab185babd3b8208a69f74d9
jukka <jukka1993@foxmail.com>
2023-10-10 19:46:36 +0800

    .

--- UrpLearn/Assets/ShaderLibrary/UnityInput.hlsl
+++ UrpLearn/Assets/ShaderLibrary/UnityInput.hlsl
@@ -3,12 +3,19 @@
 #define CUSTOM_UNITY_INPUT_INCLUDED
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
 
-//定义一个从模型空间转换到世界空间的转换矩阵
-float4x4 unity_ObjectToWorld;
-float4x4 unity_WorldToObject;
+CBUFFER_START(UnityPerDraw)
 
-//这个矩阵包含一些在这里我们不需要的转换信息
-real4 unity_WorldTransformParams;
+    //定义一个从模型空间转换到世界空间的转换矩阵
+    float4x4 unity_ObjectToWorld;
+    float4x4 unity_WorldToObject;
+
+    // float4 unity_LODFade;
+
+    //这个矩阵包含一些在这里我们不需要的转换信息
+    real4 unity_WorldTransformParams;
+
+CBUFFER_END
+
 
 // float4x4 unity_MatrixVP;
 float4x4 unity_MatrixV;
--- UrpLearn/Assets/Shaders/UnlitPass.hlsl
+++ UrpLearn/Assets/Shaders/UnlitPass.hlsl
@@ -8,7 +8,12 @@ float4 UnlitPassVertex(float3 positionOS:POSITION):SV_POSITION
     float3 positionWS = TransformObjectToWorld(positionOS.xyz);
     return TransformWorldToHClip(positionWS);
 }
-float4 _BaseColor;
+
+//所有材质的属性我们需要在常量缓冲区里定义
+CBUFFER_START(UnityPerMaterial)
+    float4 _BaseColor;
+CBUFFER_END
+
 //片元函数
 float4 UnlitPassFragment():SV_TARGET
 {
